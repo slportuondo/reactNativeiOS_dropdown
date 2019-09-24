552734bb7191e7364ffc84d9cc130ac6bd2ee2a0
Sebastian Portuondo <slportuondo@gmail.com>
2019-09-24 08:18:34 -0500

    Styled log in page

--- Session.js
+++ Session.js
@@ -25,19 +25,24 @@ class SessionController extends React.Component {
 
   render() {
     return (
-      <View style={{flex:10}}>
+      <View style={{flex:10, textAlign: 'center', alignItems: 'center'}}>
         <Header
           centerComponent={{ text: 'DROPDOWN', style: { color: '#ffa' } }}
         />
+        <View
+          style={{marginVertical: 140}}
+        ></View>
         <TextInput
+          style={{height: 40, width: 300, borderColor: 'gray', borderWidth: 1, textAlign: 'center'}}
           value = {this.state.sessionName}
           onChangeText = {this.handleNameChange}
-          placeholder = 'Enter session name'
+          placeholder = 'Session Name'
         />
         <TextInput
+          style={{height: 40, width: 300, borderColor: 'gray', borderWidth: 1, textAlign: 'center'}}
           value = {this.state.sessionKey}
           onChangeText = {this.handleKeyChange}
-          placeholder = 'Activate Session'
+          placeholder = 'Session Key'
         />
         <Button
           title='Enter Session'
